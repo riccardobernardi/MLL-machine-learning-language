f63930a2bba017f24606c6c3cdfde5b956e028cc
riccardobernardi <riccardo.bernardi@rocketmail.com>
2019-04-24 16:11:05 +0200

    uploaded to pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="MLL-machine-learning-language",
+    name="mll",
     version="0.0.1",
     author="Bernardi Riccardo",
     author_email="riccardo.bernardi@rocketmail.com",
