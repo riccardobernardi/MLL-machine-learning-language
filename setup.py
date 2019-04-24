4af36c8c1ef7f4b5495a6e13a24b80b8e015a2d5
riccardobernardi <riccardo.bernardi@rocketmail.com>
2019-04-24 15:07:17 +0200

    preparing to distribute via pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MLL-machine-learning-language",
+    version="0.0.1",
+    author="Bernardi Riccardo",
+    author_email="riccardo.bernardi@rocketmail.com",
+    description="A machine learning language that have a specific grammar made to simplify large and complex machine learning and deep learning tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/riccardobernardi/MLL-machine-learning-language",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
