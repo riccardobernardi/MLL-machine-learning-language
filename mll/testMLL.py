c3f1af058f6cbb2ee8bf33985ba2aed0ebe6dfc4
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2019-12-09 23:22:56 +0100

    Update testMLL.py

--- mll/testMLL.py
+++ mll/testMLL.py
@@ -6039,3 +6039,52 @@ class TestMLL(TestCase):
         mll = MLL(code, globals()).start()
         print(mll.get_string())
         model = mll.execute().last_model()
+
+    def test_recognition_digits_with_svm(self):
+        import matplotlib.pyplot as plt
+        from sklearn import datasets, svm, metrics
+        from sklearn.model_selection import train_test_split
+        digits = datasets.load_digits()
+
+        _, axes = plt.subplots(2, 4)
+        images_and_labels = list(zip(digits.images, digits.target))
+        for ax, (image, label) in zip(axes[0, :], images_and_labels[:4]):
+            ax.set_axis_off()
+            ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+            ax.set_title('Training: %i' % label)
+
+        n_samples = len(digits.images)
+        data = digits.images.reshape((n_samples, -1))
+
+        code = """
+        classifier : SVC with gamma=0.001
+        """
+
+        mll = MLL(code).start().execute()
+        print(mll.get_string())
+        classifier = mll.last_model()
+
+
+        # Split data into train and test subsets
+        X_train, X_test, y_train, y_test = train_test_split(
+            data, digits.target, test_size=0.5, shuffle=False)
+
+        # We learn the digits on the first half of the digits
+        classifier.fit(X_train, y_train)
+
+        # Now predict the value of the digit on the second half:
+        predicted = classifier.predict(X_test)
+
+        images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
+        for ax, (image, prediction) in zip(axes[1, :], images_and_predictions[:4]):
+            ax.set_axis_off()
+            ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+            ax.set_title('Prediction: %i' % prediction)
+
+        print("Classification report for classifier %s:\n%s\n"
+              % (classifier, metrics.classification_report(y_test, predicted)))
+        disp = metrics.plot_confusion_matrix(classifier, X_test, y_test)
+        disp.figure_.suptitle("Confusion Matrix")
+        print("Confusion matrix:\n%s" % disp.confusion_matrix)
+
+        plt.show()
